5ff4f1931641ca52fcae5c6816e9a6eafd418087
maharjun <maharjun@thoughtworks.com>
2017-10-05 20:05:24 +0530

    Taking Query Params

--- src/myFirst.js
+++ src/myFirst.js
@@ -7,9 +7,12 @@
 
 var http = require('http');
 var date = require('./datePicker');
+var url = require('url');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(req.url);
+    var params = url.parse(req.url, true).query;
+    var year = params.year + " " + params.month;
+    res.write(year);
     res.end();
 }).listen(9090);
