9313a00c05dc52a23c32e77c3bc67b0bc6a77330
maharjun <maharjun@thoughtworks.com>
2017-10-05 20:21:57 +0530

    Opening a file and writing content to it

--- src/myFirst.js
+++ src/myFirst.js
@@ -12,6 +12,21 @@ http.createServer(function (req, res) {
     fs.readFile('file.html', function (error, data) {
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write(data);
-        res.end();
     });
+    fs.appendFile('file.html', 'Maharjun', function (error, data) {
+        if (error) throw error;
+       console.log("Saved into that file");
+    });
+    fs.open('newFile.txt', 'w', function (error, file) {
+        if (error) throw error;
+        console.log("Saved new file");
+        var someContent = "This is the content";
+        fs.write(file, someContent, 0, someContent.length, null, function (error) {
+            if (error) throw error;
+            fd.close(file, function () {
+                console.log("Content is written into the file");
+            });
+        })
+    })
+    res.end();
 }).listen(9090);
