31eb557c0314b189a14a1a88740577ad186fb06f
maharjun <maharjun@thoughtworks.com>
2017-10-05 19:59:03 +0530

    Close the response after writing content to the response

--- src/myFirst.js
+++ src/myFirst.js
@@ -11,4 +11,5 @@ var date = require('./datePicker');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(req.url);
+    res.end();
 }).listen(9090);
